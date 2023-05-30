cf6e4f9161772eeb0ea96c919964ca624129cb53
Andrew Gormley <Andrew@MacBook-Pro-2.local>
2023-05-30 17:20:22 +0100

    added hover state to project links

--- app/components/Project.js
+++ app/components/Project.js
@@ -5,15 +5,20 @@ function Project(props) {
   return (
 
     <Link href={props.path}>
-      <div className="group mb-24 lg:mb-48">
-        <Image
-          className="w-full"
-          src={props.img}
-          alt={props.imgAlt}
-          priority
-          width={984}
-          height={577}
-        />
+      <div className="group mb-24 lg:mb-48 overflow-hidden">
+        <div class="relative">
+          <Image
+            className="w-full group-hover:blur-sm transition duration-300 ease-in-out"
+            src={props.img}
+            alt={props.imgAlt}
+            priority
+            width={984}
+            height={577}
+          />
+          <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
+            View project
+          </div>
+        </div>
         <div className="flex items-center mb-2 mt-6">
           <h2>{props.title} <span class="text-zinc-400">— {props.description}</span></h2>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="hidden md:block ml-2 h-4 fill-zinc-400 group-hover:fill-green-400 transition duration-150 ease-in-out">
